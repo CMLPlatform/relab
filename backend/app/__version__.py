1804f23659b716561827f2ebd97f3b428bb212c4
Simon van Lierde <simonvanlierde@gmail.com>
2025-08-14 11:51:26 +0200

    fix(backend): remove irrelevant todo

--- backend/app/__version__.py
+++ backend/app/__version__.py
@@ -1,5 +1,3 @@
 """For versioning the FastAPI app."""
-# TODO: Add instructions on setting up python-semantic-release
-# TODO: Add python-semantic-release job to Github Actions.
 
 version = "0.1.0"
