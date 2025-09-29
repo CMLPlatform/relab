74428fa3a09a51342f38f036aeed04a344f63f15
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-09-29 13:22:39 +0200

    Fix screen names for product sub screens

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,7 +5,12 @@ export default function RootLayout() {
       <Stack>
           <Stack.Screen name="index" options={{headerShown: false}}/>
           <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+
           <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
+
+          <Stack.Screen name="products/[id]/camera" options={{ headerShown: false }}/>
+          <Stack.Screen name="products/[id]/category_selection" options={{ title: "Select Category" }}/>
+          <Stack.Screen name="products/[id]/brand_selection" options={{ title: "Select Brand" }}/>
       </Stack>
   );
 }
