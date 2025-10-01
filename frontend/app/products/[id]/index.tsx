a59fc25204ccb8cbbb5e018580b9ff616ea899de
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-10-01 16:29:24 +0200

    Reload product on save for metadata

--- frontend/app/products/[id]/index.tsx
+++ frontend/app/products/[id]/index.tsx
@@ -82,7 +82,7 @@ export default function ProductPage(): JSX.Element {
         else if (id !== "new") {
             getProduct(parseInt(id)).then(setProduct);
         }
-    }, []);
+    }, [id]);
 
     useEffect(() => {
         return navigation.addListener("beforeRemove", (e) => {
