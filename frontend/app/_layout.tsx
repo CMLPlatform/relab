12e813454cd495228ebb5a0e5bd0445d834695b8
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-10-01 15:31:33 +0200

    Theming

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,22 +1,42 @@
 import { Stack } from "expo-router";
 import { DialogProvider } from "@/components/common/DialogProvider";
 import { KeyboardProvider } from 'react-native-keyboard-controller';
+import {
+    MD3LightTheme as DefaultTheme,
+    PaperProvider, useTheme,
+} from 'react-native-paper';
+
+import customTheme from '../assets/themes/light'
+
+const theme = {
+    ...DefaultTheme,
+    ...customTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        ...customTheme.colors,
+    },
+    roundness: 1,
+};
+
 
 export default function RootLayout() {
   return (
-      <KeyboardProvider>
-          <DialogProvider>
-              <Stack>
-                  <Stack.Screen name="index" options={{headerShown: false}}/>
-                  <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+      <PaperProvider theme={theme}>
+          <KeyboardProvider>
+              <DialogProvider>
 
-                  <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
+                  <Stack>
+                      <Stack.Screen name="index" options={{headerShown: false}}/>
+                      <Stack.Screen name="(tabs)" options={{title: "ReLab."}}/>
 
-                  <Stack.Screen name="products/[id]/camera" options={{ headerShown: false }}/>
-                  <Stack.Screen name="products/[id]/category_selection" options={{ title: "Select Category" }}/>
-                  <Stack.Screen name="products/[id]/brand_selection" options={{ title: "Select Brand" }}/>
-              </Stack>
-          </DialogProvider>
-      </KeyboardProvider>
+                      <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
+
+                      <Stack.Screen name="products/[id]/camera" options={{ headerShown: false }}/>
+                      <Stack.Screen name="products/[id]/category_selection" options={{ title: "Select Category" }}/>
+                      <Stack.Screen name="products/[id]/brand_selection" options={{ title: "Select Brand" }}/>
+                  </Stack>
+              </DialogProvider>
+          </KeyboardProvider>
+      </PaperProvider>
   );
 }
