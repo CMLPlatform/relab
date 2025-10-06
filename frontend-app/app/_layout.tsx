807ac232f0b6e2fe3f72665c2e71a69f5745b20f
Simon van Lierde <simonvanlierde@gmail.com>
2025-10-06 05:50:01 +0200

    refactor(frontend): Rename frontend to frontend-app and add register page

---
+++ frontend-app/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { DialogProvider } from '@/components/common/DialogProvider';
+import { Stack } from 'expo-router';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
+
+export default function RootLayout() {
+  return (
+    <KeyboardProvider>
+      <DialogProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+          <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/verify" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/reset-password" options={{ headerShown: false }} />
+
+          <Stack.Screen name="products/[id]/camera" options={{ headerShown: false }} />
+          <Stack.Screen name="products/[id]/category_selection" options={{ title: 'Select Category' }} />
+          <Stack.Screen name="products/[id]/brand_selection" options={{ title: 'Select Brand' }} />
+        </Stack>
+      </DialogProvider>
+    </KeyboardProvider>
+  );
+}
