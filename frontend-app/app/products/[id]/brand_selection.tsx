7c0239ce239e79fc28fb5da19a562dfde4dad617
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-10-11 14:25:41 +0200

    Brand list based on backend

--- frontend-app/app/products/[id]/brand_selection.tsx
+++ frontend-app/app/products/[id]/brand_selection.tsx
@@ -1,8 +1,10 @@
-import {Stack, useRouter, useNavigation, useLocalSearchParams} from "expo-router";
-import {useState} from "react";
-import {Chip, Searchbar} from 'react-native-paper';
+import {useRouter, useLocalSearchParams} from "expo-router";
+import {useEffect, useState} from "react";
+import {Searchbar} from 'react-native-paper';
 import {allBrands} from "@/services/api/fetching";
-import {ScrollView, View} from "react-native";
+import {ActivityIndicator, ScrollView, View} from "react-native";
+import {Chip} from "@/components/base";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 type searchParams = {
     id: string;
@@ -16,11 +18,15 @@ export default function BrandSelection() {
 
     // States
     const [searchQuery, setSearchQuery] = useState(preset || "");
+    const [allBrandList, setAllBrandList] = useState<string[]>([]);
 
     // Methods
-    const results = allBrands().filter(brand =>
+    const results = allBrandList.filter(brand =>
         brand.toLowerCase().includes(searchQuery.toLowerCase())
     );
+
+    // Effects
+    useEffect(() => {allBrands().then(setAllBrandList)}, []);
 
     // Callbacks
     const brandSelected = (selectedBrand: string) => {
@@ -28,12 +34,21 @@ export default function BrandSelection() {
         router.dismissTo({pathname: "/products/[id]", params: params});
     }
 
+    // Sub Render >> Brand List not loaded yet
+    if (allBrandList.length === 0) {
+        return (
+            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                <ActivityIndicator size="large"/>
+            </View>
+        );
+    }
+
     // Render
     return (
         <View style={{padding: 10}}>
             <Searchbar
                 style={{ marginVertical: 10}}
-                placeholder="Search"
+                placeholder="Search or add brand"
                 onChangeText={setSearchQuery}
                 value={searchQuery}
             />
@@ -48,7 +63,10 @@ export default function BrandSelection() {
                 }}
                 >
                     {results.includes(searchQuery) || searchQuery === "" ? null: (
-                        <Chip onPress={()=>brandSelected(searchQuery)} icon={"plus"}>
+                        <Chip
+                            onPress={()=>brandSelected(searchQuery)}
+                            icon={<MaterialCommunityIcons name={"plus"}/>}
+                        >
                             {searchQuery}
                         </Chip>)
                     }
