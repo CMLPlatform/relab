5b3cff325d4988316028146794b3946ae878e8fc
Simon van Lierde <simonvanlierde@gmail.com>
2025-10-12 13:20:22 +0200

    cicd(frontend-app): Install linting and formatting

--- frontend-app/eslint.config.js
+++ frontend-app/eslint.config.js
@@ -1,10 +1,33 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginJest = require('eslint-plugin-jest');
+const eslintPluginTestingLibrary = require('eslint-plugin-testing-library');
 
 module.exports = defineConfig([
-  expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['**/.expo/**', '**/node_modules/**', '**/dist/**'],
+  },
+
+  // Base configs
+  expoConfig,
+
+  // Import organization
+  {
+    rules: {
+      'import/order': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+
+  // Prettier integration
+  eslintPluginPrettierRecommended,
+
+  // Test files configuration
+  {
+    ...eslintPluginJest.configs['flat/recommended'],
+    ...eslintPluginTestingLibrary.configs['flat/react'],
+    files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   },
 ]);
