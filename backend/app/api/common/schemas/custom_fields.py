5f74e4517abbf75da60f91b8dbf739912c44a3f7
Simon van Lierde <simonvanlierde@gmail.com>
2025-11-08 11:48:24 +0100

    fix(backend): Use TypeAlias over simple type declaration for custom schema field types

--- backend/app/api/common/schemas/custom_fields.py
+++ backend/app/api/common/schemas/custom_fields.py
@@ -1,13 +1,13 @@
 """Shared fields for DTO schemas."""
 
-from typing import Annotated
+from typing import Annotated, TypeAlias
 
 from pydantic import AnyUrl, HttpUrl, PlainSerializer, StringConstraints
 
 # HTTP URL that is stored as string in the database.
-type HttpUrlToDB = Annotated[
+HttpUrlToDB: TypeAlias = Annotated[
     HttpUrl, PlainSerializer(lambda x: str(x), return_type=str), StringConstraints(max_length=250)
 ]
-type AnyUrlToDB = Annotated[
+AnyUrlToDB: TypeAlias = Annotated[
     AnyUrl, PlainSerializer(lambda x: str(x), return_type=str), StringConstraints(max_length=250)
 ]
