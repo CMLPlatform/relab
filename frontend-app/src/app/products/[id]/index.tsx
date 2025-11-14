e52edcba38c51a5c3847e1ac74158ab185524cc7
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-11-14 11:05:53 +0100

    Don't reset the product page one practically each navigation

--- frontend-app/src/app/products/[id]/index.tsx
+++ frontend-app/src/app/products/[id]/index.tsx
@@ -99,7 +99,7 @@ export default function ProductPage(): JSX.Element {
     } else if (id !== 'new') {
       getProduct(parseInt(id)).then(setProduct);
     }
-  }, [brand, id, isComponent, model, name, parent]);
+  }, []);
 
   useEffect(() => {
     return navigation.addListener('beforeRemove', (e) => {
