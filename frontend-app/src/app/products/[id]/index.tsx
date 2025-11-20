389996505353bc548480042b820e49aef2759b4d
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-11-20 16:11:39 +0100

    Add circularity properties to product page

--- frontend-app/src/app/products/[id]/index.tsx
+++ frontend-app/src/app/products/[id]/index.tsx
@@ -15,6 +15,7 @@ import ProductMetaData from '@/components/product/ProductMetaData';
 import ProductPhysicalProperties from '@/components/product/ProductPhysicalProperties';
 import ProductTags from '@/components/product/ProductTags';
 import ProductType from '@/components/product/ProductType';
+import ProductCircularityProperties from '@/components/product/ProductCircularityProperties';
 
 import { useDialog } from '@/components/common/DialogProvider';
 
@@ -128,6 +129,10 @@ export default function ProductPage(): JSX.Element {
     setProduct({ ...product, physicalProperties: newProperties });
   };
 
+  const onChangeCircularityProperties = (newProperties: typeof product.circularityProperties) => {
+    setProduct({ ...product, circularityProperties: newProperties });
+  };
+
   const onBrandChange = (newBrand: string) => {
     setProduct({ ...product, brand: newBrand });
   };
@@ -235,6 +240,11 @@ export default function ProductPage(): JSX.Element {
           product={product}
           editMode={editMode}
           onChangePhysicalProperties={onChangePhysicalProperties}
+        />
+        <ProductCircularityProperties
+          product={product}
+          editMode={editMode}
+          onChangeCircularityProperties={onChangeCircularityProperties}
         />
         <ProductComponents product={product} editMode={editMode} />
         <ProductMetaData product={product} />
