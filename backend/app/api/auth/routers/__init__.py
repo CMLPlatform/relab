cc5c3793d9fc7e0c9379fe829c20aa8bab5e4206
Simon van Lierde <simonvanlierde@gmail.com>
2026-05-04 18:09:50 +0200

    fix(backend): move browser pages out of versioned api

--- backend/app/api/auth/routers/__init__.py
+++ backend/app/api/auth/routers/__init__.py
@@ -2,14 +2,12 @@
 
 from .admin import all_routers as admin_routers
 from .auth import router as auth_router
-from .frontend import router as frontend_router
 from .oauth import router as oauth_router
 from .users import public_profile_router
 from .users import router as user_router
 
 all_routers = [
     auth_router,
-    frontend_router,
     oauth_router,
     user_router,
     public_profile_router,
