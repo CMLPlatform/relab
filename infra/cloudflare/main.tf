21249a54c06fae6b45d943ee3bd66501bd045976
Simon van Lierde <simonvanlierde@gmail.com>
2026-05-06 13:30:56 +0200

    fix(infra): enforce Cloudflare edge TLS baseline

--- infra/cloudflare/main.tf
+++ infra/cloudflare/main.tf
@@ -18,6 +18,24 @@ resource "cloudflare_dns_record" "edge" {
   comment = "RELab ${var.environment} ${each.key} edge route managed by OpenTofu."
 }
 
+resource "cloudflare_zone_setting" "minimum_tls_version" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "min_tls_version"
+  value      = "1.2"
+}
+
+resource "cloudflare_zone_setting" "tls_1_3" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "tls_1_3"
+  value      = "on"
+}
+
+resource "cloudflare_zone_setting" "always_use_https" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "always_use_https"
+  value      = "on"
+}
+
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "relab" {
   account_id = var.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.relab.id
